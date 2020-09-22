e050cf8cda166f5c2b073ca7d293238b9c36af44
Baptiste-Caral <baptiste.caral@gmail.com>
2020-09-23 01:17:41 +0200

    carousel

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,150 +1,145 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { gsap } from 'gsap'
 import img from '../assets/images/img.png'
 import pdf from '../assets/images/pdf.png'
 import timer from '../assets/images/timer.png'
-import reactSvg from '../assets/images/React-icon.svg'
 import velo from '../assets/images/velo.png'
-import { FaBeer } from 'react-icons/fa';
+import Slider from 'infinite-react-carousel';
+
 
 function Projects() {
 
- let project1 = {
-   image: img,
-   screenshot: '',
-   title: 'oZone',
-   techno: 'Redux',
-   subtechno: 'école O\'Clock',
-   description: 'Projet écologique.',
-   linkGitHub: 'https://github.com/Baptiste-Caral',
-   linkProject: 'https://app.elizay.com'
- }
- let project2 = {
-
-  image: pdf,
-  screenshot: '',
-  title: 'Générateur de Pdf',
-  techno: 'Hooks',
-  subtechno: 'Mission Freelance',
-  description: 'Gestion de patrimoine.',
-  linkGitHub: 'https://github.com/Baptiste-Caral',
-  linkProject: 'https://app.elizay.com'
-}
-let project3 = {
-
-  image: timer,
-  screenshot: '',
-  title: 'Timer',
-  techno: 'Pure Vanilla Js',
-  subtechno: 'Projet perso',
-  description: 'Neumorphism.',
-  linkGitHub: 'https://github.com/Baptiste-Caral',
-  linkProject: 'https://app.elizay.com'
-}
-let project4 = {
-
-  image: velo,
-  screenshot: '',
-  title: 'Movie & Me',
-  techno: 'React-Native',
-  subtechno: 'Projet perso',
-  description: 'App Mobile.',
-  linkGitHub: 'https://github.com/Baptiste-Caral',
-  linkProject: 'https://app.elizay.com'
-}
-const initialState = {
-
-  image: reactSvg,
-  screenshot: '',
-  title: 'Projets',
-  techno: 'Javascript',
-  subtechno: 'React',
-  description: 'Front-end.',
-  linkGitHub: 'https://github.com/Baptiste-Caral',
-  linkProject: 'https://app.elizay.com'
-}
- const [state, setState] = useState(initialState)
- const [classActive, setClassActive] = useState(true)
-
-const handleOnClick = (index) => {
+  const [width, setWidth] = useState(window.innerWidth)
+  let rows = 4
+  useEffect(() => {
+    const handleResize = () =>setWidth(window.innerWidth)
+    window.addEventListener('resize', handleResize)
+    console.log(width);
+  },[width])
   
-  let project = state;
-  if (index === 0) {
-    project = initialState
-  } else if (index === 1) {
-    project = project1
-  } else if (index === 2) {
-    project = project2
-  } else if (index === 3) {
-    project = project3
-  } else if (index === 4) {
-    project = project4
-  }   
-  setState(project);
-  setClassActive(true)
-  gsap.fromTo('.projects_image',{duration: 1, x:450,y:200}, {duration: 1, x:0, y:0})
-}
-const handleOnmouseLeave = () => {
+  if(width < 980) {
+    rows = 3
+  } 
+  if (width < 780) {
+    rows = 2
+  }
+  if (width < 480) {
+    rows = 1
+  }
+
+
   
-  setClassActive(false)
-  gsap.fromTo('.projects_image', {duration: 1, x:0, y:0}, {duration: 1, x:450,y:200})
-  // gsap.to('.projects_image', {duration: 1, x:-150})
+  const projects = [{
+    id: 1,
+    image: img,
+    screenshot: '',
+    title: 'oZone',
+    techno: 'Redux',
+    subtechno: 'école O\'Clock',
+    description: 'Projet écologique.',
+    linkGitHub: 'https://github.com/Baptiste-Caral',
+    linkProject: 'https://app.elizay.com'
+  },
+  {
+    id: 2,
+    image: pdf,
+    screenshot: '',
+    title: 'Générateur de Pdf',
+    techno: 'React Hooks',
+    subtechno: 'Mission Freelance',
+    description: 'Gestion de patrimoine.',
+    linkGitHub: 'https://github.com/Baptiste-Caral',
+    linkProject: 'https://app.elizay.com'
+  },
+  {
+    id: 3,
+    image: timer,
+    screenshot: '',
+    title: 'Timer',
+    techno: 'Pure Vanilla Js',
+    subtechno: 'Projet perso',
+    description: 'Neumorphism.',
+    linkGitHub: 'https://github.com/Baptiste-Caral',
+    linkProject: 'https://app.elizay.com'
+  },
+  {
+    id: 4,
+    image: velo,
+    screenshot: '',
+    title: 'Movie & Me',
+    techno: 'React-Native',
+    subtechno: 'Projet perso',
+    description: 'App Mobile.',
+    linkGitHub: 'https://github.com/Baptiste-Caral',
+    linkProject: 'https://app.elizay.com'
+  },
+  
+]
+
+const [index, setIndex] = useState(0)
+const [index2, setIndex2] = useState(0)
+
+const handleOnClick = (index) => { 
+  
+  
+  gsap.fromTo('.projects_image', {duration: .5, x:0, y:0}, {duration: .5, x:850,y:300})
+  gsap.fromTo('.projects_headertruc', {duration: .5, x:0, y:0}, {duration: 1, x:-650,y:0})
+  
+  setIndex2(index);
+  setTimeout(() => {
+
+    setIndex(index);
+    
+    gsap.fromTo('.projects_image',{duration: .5, x:850,y:200}, {duration: .5, x:0, y:0})
+    gsap.fromTo('.projects_headertruc',{duration: .5, x:-650,y:0}, {duration: .5, x:0, y:0})
+  }, 550);
 }
 
-
-  return (
-
-    <div className="projects">
+const settings =  {
+  autoplay: true,
+  adaptiveHeight: true,
+  slidesToShow: rows,
+  dots: true,
+  
+};
+return (
+  
+  <div className="projects">
       {/* Head & Image container */}
-      <div >
-        <div className={`projects_header ${classActive ? "projects_header_in" : "projects_header_out"}`}>
-          <p>
-            <span className="projects_title">{state.title}</span>
-            <span className="projects_subtitle">{state.description}</span>
-            <span className="projects_techno">{state.techno}</span>
-            <span className="projects_subtechno">{state.subtechno}</span>
+      <div className="projects_header_container">
+        <div className="projects_header">
+          <p className="projects_headertruc">
+            <span className="projects_title">{projects[index].title}</span>
+            <span className="projects_subtitle">{projects[index].description}</span>
+            <span className="projects_techno">{projects[index].techno}</span>
+            <span className="projects_subtechno">{projects[index].subtechno}</span>
           </p>
-        </div>
           <div className="projects_image_wrapper">
             <div className="projects_image_container">
-              <img className="projects_image" src={state.image} alt={state.image}/> 
-              {/* <img className={`projects_image ${classActive ? "projects_image_in" : "projects_image_out"}`} src={state.image} alt={state.image}/>  */}
-            </div>
-            
+              <img className="projects_image" src={projects[index].image} alt={projects[index].image}/> 
+            </div>  
           </div>
+        </div>
       </div>
       {/* Projects container */}
-      <div className="projects_container">
-        <div className="project_container">
-          <div className="project_content" onClick={() => handleOnClick(1)} onMouseLeave={() => handleOnmouseLeave()}>
-            <h3>{project1.title}</h3>
-            <h5><a href={project1.linkProject}>Visiter le site</a></h5>
-            <h5><a href={project1.linkGitHub}>Voir le projet sur gitHub</a></h5>
-            <p><FaBeer /></p>
-            
-          </div>
-        </div>
-        <div className="project_container">
-          <div className="project_content" onClick={() => handleOnClick(2)} onMouseLeave={() => handleOnmouseLeave()}>
-            <h3>{project2.title}</h3>
-            <h5><a href={project1.linkProject}>Visiter le site</a></h5>
-            <h5><a href={project1.linkGitHub}>Voir le projet sur gitHub</a></h5>
-          </div>
-        </div>
-        <div className="project_container" >
-          <div className="project_content" onClick={() => handleOnClick(3)} onMouseLeave={() => handleOnmouseLeave()}>
-            <h3>{project3.title}</h3>
-            <h5><a href={project1.linkProject}>Visiter le site</a></h5>
-            <h5><a href={project1.linkGitHub}>Voir le projet sur gitHub</a></h5>
-          </div>
-        </div>
-        <div className="project_container">
-          <div className="project_content" onClick={() => handleOnClick(4)} onMouseLeave={() => handleOnmouseLeave()}>
-            <h3>{project4.title}</h3>
-            <h5><a href={project1.linkProject}>Visiter le site</a></h5>
-            <h5><a href={project1.linkGitHub}>Voir le projet sur gitHub</a></h5>
-          </div>
-        </div> 
+    
+      <div >
+        <Slider beforeChange={(index) => handleOnClick(index)} { ...settings }>         
+          {projects.map((project, i) => 
+            <div key={i} className="project_container">
+              {index2 !== i && <div className="project_content" onClick={() => handleOnClick(i)}>
+                <h3>{project.title}</h3>
+                <h5><a href={project.linkProject}>Visiter le site</a></h5>
+                <h5><a href={project.linkGitHub}>Voir le projet sur gitHub</a></h5>
+              </div>}
+              {index2 === i&& <div className="project_content2" onClick={() => handleOnClick(i)}>
+                <h3>{project.title}</h3>
+                <h5><a href={project.linkProject}>Visiter le site</a></h5>
+                <h5><a href={project.linkGitHub}>Voir le projet sur gitHub</a></h5>
+              </div>}
+            </div>
+          )}
+        </Slider>
       </div>
     </div>
   );
